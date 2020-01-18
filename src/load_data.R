c54b4e21dec4d63e4af3797de1c0f2e1b49dc0e3
jake1125 <agdal1125@gmail.com>
2020-01-17 20:15:28 -0800

    updated script -added file writing & testing function

--- src/load_data.R
+++ src/load_data.R
@@ -3,26 +3,81 @@
 
 "This script downloads and loads dataset given url.
 
-Usage: load_data.R <arg1>
+Usage: load_data.R [--file_dir=<file_dir> --file_name=<file_name>]
 
 Options:
-<arg>             Takes any path type containing dataframe (this is a required positional argument)
+  --file_dir=<file_dir>     Path to save your datafile [default: ../data/]
+  --file_name=<file_name>    your datafile name [default: quebec_city_airbnb_data.csv]
 " -> doc
 
 library(docopt)
 library(tidyverse)
+library(testthat)
 
 opt <- docopt(doc)
+# print the options for checking
+print(head(opt)) 
 
-print(head(opt))
-#file <- read_csv(opt)
-file <- read_csv("http://data.insideairbnb.com/canada/qc/quebec-city/2019-11-07/data/listings.csv.gz")
 
-airbnb_df <- subset(file, select = -c(summary, space, description, scrape_id, last_scraped, 
-                                      experiences_offered, thumbnail_url, medium_url, xl_picture_url, 
-                                      host_acceptance_rate, name, neighbourhood, neighborhood_overview, 
-                                      neighbourhood_group_cleansed, license, notes, transit, access, 
-                                      interaction, house_rules, picture_url, host_about, host_thumbnail_url, 
-                                      host_picture_url))
-head(airbnb_df)
+main <- function(file_dir, file_name){
+  
+  # Read Data from source
+  file <- read_csv("http://data.insideairbnb.com/canada/qc/quebec-city/2019-11-07/data/listings.csv.gz")
+  
+  # Import necessary columns
+  
+  airbnb_df <- subset(file, select = -c(summary, space, description, scrape_id, last_scraped, 
+                                        experiences_offered, thumbnail_url, medium_url, xl_picture_url, 
+                                        host_acceptance_rate, name, neighbourhood, neighborhood_overview, 
+                                        neighbourhood_group_cleansed, license, notes, transit, access, 
+                                        interaction, house_rules, picture_url, host_about, host_thumbnail_url, 
+                                        host_picture_url))
+  
+  
+  
+  # Write Data to a csv file
+  if (endsWith(file_dir,"/")){
+    path <- paste(file_dir,file_name, sep="")
+  } else {
+    path <- paste(file_dir, file_name, sep="/")
+  }
+  
+  write_csv(airbnb_df, path)
+  print("---------------------------")
+  print("---------------------------")
+  print("Data Successfully Downloaded.")
+  print("---------------------------")
+  print("---------------------------")
+  
+  # Test function
+  test_data(data_dir=path)
 
+}
+
+
+#' Test downloaded dataset is the identical with the one used in our analysis
+#'
+#' @param  path path of downloaded datafile
+#' @return the string that confirmss consistency of data through a dimension check.
+
+test_data <- function(data_dir){
+  data <- read_csv(data_dir)
+  
+  # Test 1
+  test_that("Number of columns should be 82", {
+    expect_equal(dim(data)[2], 82)
+  })
+  # Test 2
+  test_that("Number of observations should be 2704", {
+    expect_equal(dim(data)[1], 2704)
+  })
+  
+  # Return a message
+  print("--------------")
+  print("--------------")
+  print("Data confirmed.")
+  print("--------------")
+  print("--------------")
+}
+
+main(opt$file_dir, opt$file_name)
